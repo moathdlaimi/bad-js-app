9c853f9f7969b36f4654a699035dff718a2f5b68
moathdlaimi <64281168+moathdlaimi@users.noreply.github.com>
2024-05-07 23:42:47 -0500

    updated the searchProduct function

--- data/static/codefixes/dbSchemaChallenge_1.ts
+++ data/static/codefixes/dbSchemaChallenge_1.ts
@@ -1,17 +1,28 @@
-module.exports = function searchProducts () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
-      .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
-        for (let i = 0; i < products.length; i++) {
-          products[i].name = req.__(products[i].name)
-          products[i].description = req.__(products[i].description)
-        }
-        res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
-        next(error.parent)
-      })
-  }
-}
+module.exports = function searchProducts() {
+  return function(req, res, next) {
+    let criteria = req.query.q === 'undefined' ? '' : req.query.q || '';
+    criteria = criteria.length <= 200 ? criteria : criteria.substring(0, 200);
+
+    models.Products.findAll({
+      where: {
+        [Op.or]: [
+          { name: { [Op.like]: `%${criteria}%` } },
+          { description: { [Op.like]: `%${criteria}%` }
+        ],
+        deletedAt: null
+      },
+      order: [['name', 'ASC']]
+    })
+    .then(products => {
+      const localProducts = products.map(product => ({
+        name: req.__(product.name),
+        description: req.__(product.description)
+      }));
+      
+      res.json(localProducts);
+    })
+    .catch(error => {
+      next(error);
+    });
+  };
+};
